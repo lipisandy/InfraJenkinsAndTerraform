6f1be4d68ba29f3944c252a072bd10da0cf88d8c
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-10-07 13:05:08 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
   
   versioning {
     enabled = true
